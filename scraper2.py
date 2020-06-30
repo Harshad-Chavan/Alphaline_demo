a924c29b2932cd5acbcc0013108c9c9421b1dc97
Harshad-desktop <harshadchavan141@gmail.com>
2020-07-01 01:21:49 +0530

    finished the basic log
    finished the basic logic of fetching the prices from the website and computing the difference

--- scraper2.py
+++ scraper2.py
@@ -3,12 +3,13 @@ import mimetypes
 import json
 import re
 from bs4 import BeautifulSoup
+import time
 
 
 names_list = []
 
 def get_securities(company_name):
-
+    securities = {}
     #NSE connection settings
     conn_nse = http.client.HTTPSConnection("www.nseindia.com")
     payload_nse = ''
@@ -24,14 +25,6 @@ def get_securities(company_name):
 
     #Fetching all securities with name containing the given string
 
-    #fetching from nse 
-    conn_nse.request("GET", f"/api/search/autocomplete?q={company_name}", payload_nse, headers_nse)
-    res = conn_nse.getresponse()
-    data = res.read()
-    symbols_json = json.loads(data.decode("utf-8"))
-    nse_list = [element["symbol"] for element in symbols_json["symbols"]]
-    print(nse_list)
-
     #fetching from bse
     bse_list = []
     conn_bse.request("GET", f"/Msource/1D/getQouteSearch.aspx?Type=EQ&text={company_name}&flag=nw", payload_bse, headers_bse)
@@ -41,53 +34,71 @@ def get_securities(company_name):
     l = parsed_html.find_all('span',attrs = {'class' : ''})
     for sym in l:
         ns = (str(sym).replace("<span>","").replace("</span>","").replace("<strong>","").replace("</strong>",""))
-        ls = re.search('(\w*)',ns).group(1)
-        bse_list.append(ls)
+        symbols = re.search('^([A-Z-0-9]*)',ns).group(1)
+        scripcodes = re.search('([A-Z-0-9]*)$',ns).group(1)
+        bse_list.append((symbols,scripcodes))
+        securities[symbols] = scripcodes
+    print(bse_list)
+
+    #fetching from nse 
+    conn_nse.request("GET", f"/api/search/autocomplete?q={company_name}", payload_nse, headers_nse)
+    res = conn_nse.getresponse()
+    data = res.read()
+    symbols_json = json.loads(data.decode("utf-8"))
+    for element in symbols_json["symbols"]:
+        if element['symbol'] in list(securities.keys()):
+            print("present")
+        else:
+            print("not present")
 
     #returning only the common securities on both the exchanges
-    return (set(nse_list) & set(bse_list))
+    return securities
 
 
+def compute_difference(temp,selected_symbol):
+    #fetching nse price
+    nse_price = float(get_nse_price(selected_symbol))
+    #fetching bse price
+    bse_price = float(get_bse_price(temp[selected_symbol]))
+    maxm = max(nse_price,bse_price)
+    minm = min(nse_price,bse_price)
+    print(f"difference in price == : { maxm - minm }")
+    
 
+def get_nse_price(company_name):
+    conn = http.client.HTTPSConnection("www.nseindia.com")
+    payload = ''
+    headers = {
+            'Cookie': ''
+            }
+    conn.request("GET", f"/api/quote-equity?symbol={company_name}", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    jdict = json.loads(data.decode("utf-8"))
+    print(jdict["priceInfo"]["close"])
+    return jdict["priceInfo"]["close"]
 
+def get_bse_price(scripcode):
+    conn = http.client.HTTPSConnection("api.bseindia.com")
+    payload = ''
+    headers = {}
 
-
-
-
-# def get_nse_price(company_name):
-#     conn = http.client.HTTPSConnection("www.nseindia.com")
-#     payload = ''
-#     headers = {
-#             'Cookie': ''
-#             }
-#     name_list = get_securities(company_name,conn,payload,headers)
-#     print(name_list)
-#     conn.request("GET", f"/api/quote-equity?symbol={name_list[0]}", payload, headers)
-#     res = conn.getresponse()
-#     data = res.read()
-#     jdict = json.loads(data.decode("utf-8"))
-#     print(jdict["priceInfo"]["close"])
-#     return jdict["priceInfo"]["close"]
-
-# def get_bse_price(company_name):
-#     conn = http.client.HTTPSConnection("api.bseindia.com")
-#     payload = ''
-#     headers = {}
-
-#     conn.request("GET", "/BseIndiaAPI/api/StockReachGraph/w?scripcode=500209&flag=0&fromdate=&todate=&seriesid=", payload, headers)
-#     res = conn.getresponse()
-#     data = res.read()
-#     jdict = json.loads(data.decode("utf-8"))
-#     print(jdict["CurrVal"])
-#     return jdict["CurrVal"]
+    conn.request("GET", f"/BseIndiaAPI/api/StockReachGraph/w?scripcode={scripcode}&flag=0&fromdate=&todate=&seriesid=", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    jdict = json.loads(data.decode("utf-8"))
+    print(jdict["CurrVal"])
+    return jdict["CurrVal"]
 
 
 company_name = input("enter company name::")
-print(get_securities(company_name.lower()))
+securities = get_securities(company_name.lower())
+symbol_name = input("select a symbol from above list::")
+
+i = 1
+while i < 10:
+    time.sleep(1)
+    compute_difference(securities,symbol_name)
+    i += 1
 
 
-# i = 1
-# while i < 10:
-#    result =  float(get_nse_price(company_name)) - float(get_bse_price("INFY"))
-#    print(result)
-#    i += 1
