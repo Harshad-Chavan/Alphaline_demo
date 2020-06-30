1efd313c5270b3e2cef44c887011bb6d5373f51b
Harshad-desktop <harshadchavan141@gmail.com>
2020-07-01 01:32:57 +0530

    created a function to generate connection

--- scraper2.py
+++ scraper2.py
@@ -8,27 +8,35 @@ import time
 
 names_list = []
 
-def get_securities(company_name):
-    securities = {}
+def get_conn(site):
     #NSE connection settings
-    conn_nse = http.client.HTTPSConnection("www.nseindia.com")
-    payload_nse = ''
-    headers_nse = {
+    if site == "nse":
+        conn_nse = http.client.HTTPSConnection("www.nseindia.com")
+        payload_nse = ''
+        headers_nse = {
             'Cookie': ''
             }
-
+        return conn_nse,payload_nse,headers_nse
     #BSE connection settings
-    conn_bse = http.client.HTTPSConnection("api.bseindia.com")
-    payload_bse = ''
-    headers_bse = {}
+    elif site == 'bse':
+        conn_bse = http.client.HTTPSConnection("api.bseindia.com")
+        payload_bse = ''
+        headers_bse = {}
+        return conn_bse,payload_bse,headers_bse
 
 
+def get_securities(company_name):
+    #Dictionary to maintain the symbol name and scrip codes
+    # key -> symbol value --> scripcode
+    securities = {}
+    
     #Fetching all securities with name containing the given string
 
     #fetching from bse
     bse_list = []
-    conn_bse.request("GET", f"/Msource/1D/getQouteSearch.aspx?Type=EQ&text={company_name}&flag=nw", payload_bse, headers_bse)
-    res = conn_bse.getresponse()
+    conn,payload,headers = get_conn('bse')
+    conn.request("GET", f"/Msource/1D/getQouteSearch.aspx?Type=EQ&text={company_name}&flag=nw", payload, headers)
+    res = conn.getresponse()
     data = res.read()
     parsed_html = BeautifulSoup(data,'html.parser')
     l = parsed_html.find_all('span',attrs = {'class' : ''})
@@ -41,8 +49,9 @@ def get_securities(company_name):
     print(bse_list)
 
     #fetching from nse 
-    conn_nse.request("GET", f"/api/search/autocomplete?q={company_name}", payload_nse, headers_nse)
-    res = conn_nse.getresponse()
+    conn,payload,headers = get_conn('nse')
+    conn.request("GET", f"/api/search/autocomplete?q={company_name}", payload, headers)
+    res = conn.getresponse()
     data = res.read()
     symbols_json = json.loads(data.decode("utf-8"))
     for element in symbols_json["symbols"]:
@@ -60,17 +69,14 @@ def compute_difference(temp,selected_symbol):
     nse_price = float(get_nse_price(selected_symbol))
     #fetching bse price
     bse_price = float(get_bse_price(temp[selected_symbol]))
+    
     maxm = max(nse_price,bse_price)
     minm = min(nse_price,bse_price)
     print(f"difference in price == : { maxm - minm }")
     
 
 def get_nse_price(company_name):
-    conn = http.client.HTTPSConnection("www.nseindia.com")
-    payload = ''
-    headers = {
-            'Cookie': ''
-            }
+    conn,payload,headers = get_conn('nse')
     conn.request("GET", f"/api/quote-equity?symbol={company_name}", payload, headers)
     res = conn.getresponse()
     data = res.read()
@@ -79,10 +85,7 @@ def get_nse_price(company_name):
     return jdict["priceInfo"]["close"]
 
 def get_bse_price(scripcode):
-    conn = http.client.HTTPSConnection("api.bseindia.com")
-    payload = ''
-    headers = {}
-
+    conn,payload,headers = get_conn('bse')
     conn.request("GET", f"/BseIndiaAPI/api/StockReachGraph/w?scripcode={scripcode}&flag=0&fromdate=&todate=&seriesid=", payload, headers)
     res = conn.getresponse()
     data = res.read()
